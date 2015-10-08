981205a3aeba04d8609499a102af9e337a8c7ead
Clint.zhang <hustzza@gmail.com>
2015-10-08 10:43:52 +0100

    mpu

--- mpu.py
+++ mpu.py
@@ -1,3 +1,4 @@
+'''Memory Protection Unit support.'''
 import sys
 
 class MPUException(Exception):
@@ -13,6 +14,9 @@ class MPUException(Exception):
 
 class OrderedDict(object):
     '''Ordered dictionary'''
+    # pylint: disable=too-few-public-methods
+    # (all our work is in overloaded operators, which are basically public)
+
     def __init__(self, items):
         self.items = items
         self.keys = []
@@ -45,10 +49,14 @@ MPU_END_ADDRESS = 1
 IF_START_PREDEFINED = 2
 IF_END_PREDEFINED = 3
 
-class MPU_info(object):
+class MPUInfo(object):
     '''MPU information'''
     __instance = None
-    def __init__(self):
+    __magic = object()
+    def __init__(self, token):
+        if token != MPUInfo.__magic:
+            raise MPUException("Internal error")
+
         self.isEnabled = OrderedDict([('GRAM_READONLY', False), ('GRAM_NO_DCP', False),
                                       ('GRAM_EDC', False), ('GRAM_HOST', False),
                                       ('GRAM_MCP_0', False), ('GRAM_MCP_1', False), ('GRAM_MCP_2', False),
@@ -61,32 +69,32 @@ class MPU_info(object):
 
         #First boolean: if first argument exsits
         #Second boolean: if second argument exsits
-        #bufName means the to be protected buffers defined in C
-        self.bufName = {'GRAM_MCP_0':["", "", False, False], 'GRAM_MCP_1':["", "", False, False],
-                        'GRAM_MCP_2':["", "", False, False], 'GRAM_MCP_3':["", "", False, False],
-                        'GRAM_MCP_4':["", "", False, False], 'GRAM_MCP_5':["", "", False, False],
-                        'GRAM_FFT_0':["", "", False, False], 'GRAM_FFT_1':["", "", False, False],
-                        'GRAM_FFT_2':["", "", False, False], 'GRAM_FFT_3':["", "", False, False],
-                        'GRAM_FFT_4':["", "", False, False], 'GRAM_FFT_5':["", "", False, False],
-                        'GRAM_READONLY':["", "", False, False], 'GRAM_MCU_ECU_HOLE':["", "", False, False],
-                        'GRAM_NO_DCP':["", "", False, False], 'GRAM_EDC':["", "", False, False],
-                        'GRAM_HOST':["", "", False, False], 'EXTRAM_READONLY':["", "", False, False],
-                        'EXTRAM_MCU_ECU_HOLE':["", "", False, False], 'EXTRAM_NO_DCP':["", "", False, False],
-                        'EXTRAM_EDC':["", "", False, False], 'EXTRAM_HOST':["", "", False, False]}
+        #protectedBuffers means the to be protected buffers defined in C
+        self.protectedBuffers = {'GRAM_MCP_0':["", "", False, False], 'GRAM_MCP_1':["", "", False, False],
+                                 'GRAM_MCP_2':["", "", False, False], 'GRAM_MCP_3':["", "", False, False],
+                                 'GRAM_MCP_4':["", "", False, False], 'GRAM_MCP_5':["", "", False, False],
+                                 'GRAM_FFT_0':["", "", False, False], 'GRAM_FFT_1':["", "", False, False],
+                                 'GRAM_FFT_2':["", "", False, False], 'GRAM_FFT_3':["", "", False, False],
+                                 'GRAM_FFT_4':["", "", False, False], 'GRAM_FFT_5':["", "", False, False],
+                                 'GRAM_READONLY':["", "", False, False], 'GRAM_MCU_ECU_HOLE':["", "", False, False],
+                                 'GRAM_NO_DCP':["", "", False, False], 'GRAM_EDC':["", "", False, False],
+                                 'GRAM_HOST':["", "", False, False], 'EXTRAM_READONLY':["", "", False, False],
+                                 'EXTRAM_MCU_ECU_HOLE':["", "", False, False], 'EXTRAM_NO_DCP':["", "", False, False],
+                                 'EXTRAM_EDC':["", "", False, False], 'EXTRAM_HOST':["", "", False, False]}
 
         self.mask = 0x00000000
 
         self.enabledRegions = []
 
     @staticmethod
-    def get():
-        if MPU_info.__instance == None:
-            MPU_info.__instance = MPU_info()
-        return MPU_info.__instance
+    def Get():
+        '''Return the single instance of this class.'''
+        if MPUInfo.__instance == None:
+            MPUInfo.__instance = MPUInfo(MPUInfo.__magic)
+        return MPUInfo.__instance
 
-    def VerifyRegions(self, p, isGRAM):
-        '''Verifing whether the enabled region has been defined or not. If yes, it returns the buffers that have been enabled
-            mask contains the enabledRegions information'''
+    def VerifyRegions(self, part, isGRAM):
+        '''Verifing whether the enabled region has been defined or not'''
 
         gramEnabledRegions = []
         extramEnabledRegions = []
@@ -106,31 +114,34 @@ class MPU_info(object):
         #If the region enabled is not appended, throw exception
         if isGRAM:
             for gramEnabledRegion in gramEnabledRegions:
-                if not self.bufName[gramEnabledRegion][MPU_START_ADDRESS] \
-                in (p.dbl + p.cpx + p.sxt + p.pkd + none) \
-                and not self.bufName[gramEnabledRegion][IF_START_PREDEFINED]:
+                if not self.protectedBuffers[gramEnabledRegion][MPU_START_ADDRESS] \
+                in (part.dbl + part.cpx + part.sxt + part.pkd + none) \
+                and not self.protectedBuffers[gramEnabledRegion][IF_START_PREDEFINED]:
                     raise MPUException("gram region start address " + "[" +
-                                       str(self.bufName[gramEnabledRegion][MPU_START_ADDRESS]) + "]" + " not appended  ")
+                                       str(self.protectedBuffers[gramEnabledRegion][MPU_START_ADDRESS]) +
+                                       "]" + " not appended  ")
 
-                if not self.bufName[gramEnabledRegion][MPU_END_ADDRESS] \
-                in (p.dbl + p.cpx + p.sxt + p.pkd + noend + none) \
-                and not self.bufName[gramEnabledRegion][IF_END_PREDEFINED]:
+                if not self.protectedBuffers[gramEnabledRegion][MPU_END_ADDRESS] \
+                in (part.dbl + part.cpx + part.sxt + part.pkd + noend + none) \
+                and not self.protectedBuffers[gramEnabledRegion][IF_END_PREDEFINED]:
                     raise MPUException("gram region end address " + "[" +
-                                       str(self.bufName[gramEnabledRegion][MPU_END_ADDRESS]) + "]" + " not appended  ")
+                                       str(self.protectedBuffers[gramEnabledRegion][MPU_END_ADDRESS]) + "]" + " not appended  ")
 
         elif not isGRAM:
             for extramEnabledRegion in extramEnabledRegions:
-                if not self.bufName[extramEnabledRegion][MPU_START_ADDRESS] \
-                in (p.cached + p.uncached + none) \
-                and not self.bufName[extramEnabledRegion][IF_START_PREDEFINED]:
-                     raise MPUException("extram region start address " + "[" +
-                                        str(self.bufName[extramEnabledRegion][MPU_START_ADDRESS]) + "]" + " not appended  ")
+                if not self.protectedBuffers[extramEnabledRegion][MPU_START_ADDRESS] \
+                in (part.cached + part.uncached + none) \
+                and not self.protectedBuffers[extramEnabledRegion][IF_START_PREDEFINED]:
+                    raise MPUException("extram region start address " + "[" +
+                                       str(self.protectedBuffers[extramEnabledRegion][MPU_START_ADDRESS]) + "]" +
+                                       " not appended  ")
 
-                if not self.bufName[extramEnabledRegion][MPU_END_ADDRESS] \
-                in (p.cached + p.uncached + noend + none)\
-                and not self.bufName[extramEnabledRegion][IF_END_PREDEFINED]:
-                     raise MPUException("extram region end address " + "[" +
-                                        str(self.bufName[extramEnabledRegion][MPU_END_ADDRESS]) + "]" + " not appended  ")
+                if not self.protectedBuffers[extramEnabledRegion][MPU_END_ADDRESS] \
+                in (part.cached + part.uncached + noend + none)\
+                and not self.protectedBuffers[extramEnabledRegion][IF_END_PREDEFINED]:
+                    raise MPUException("extram region end address " + "[" +
+                                       str(self.protectedBuffers[extramEnabledRegion][MPU_END_ADDRESS]) + "]" +
+                                       " not appended  ")
 
     def SetRegionAndMask(self):
         '''Return enabled buffers and mask'''
@@ -143,57 +154,56 @@ class MPU_info(object):
             i += 1
 
 
-mpu_info = MPU_info.get()
-
 def _enableRegion(region, start, end=None):
     '''Enable region'''
-    global mpu_info
+    mpuInfo = MPUInfo.Get()
 
     if start == None:
-        mpu_info.isEnabled[region] = True
-        mpu_info.bufName[region][MPU_START_ADDRESS] = "None"
-        mpu_info.bufName[region][MPU_END_ADDRESS] = "None"
+        mpuInfo.isEnabled[region] = True
+        mpuInfo.protectedBuffers[region][MPU_START_ADDRESS] = "None"
+        mpuInfo.protectedBuffers[region][MPU_END_ADDRESS] = "None"
         return
 
-    if not region in mpu_info.isEnabled.keys:
+    if not region in mpuInfo.isEnabled.keys:
         sys.stderr.write("mpu: Error:Region "+ region + " not defined  ")
         exit(1)
 
-    for existRegion in mpu_info.isEnabled.keys:
-        if mpu_info.isEnabled[existRegion]:
-            if mpu_info.bufName[existRegion][MPU_START_ADDRESS] == start \
-            or mpu_info.bufName[existRegion][MPU_END_ADDRESS] == start:
-                mpu_info.bufName[region][IF_START_PREDEFINED] = True
-                mpu_info.bufName[region][MPU_START_ADDRESS] = existRegion
+    for existRegion in mpuInfo.isEnabled.keys:
+        if mpuInfo.isEnabled[existRegion]:
+            if mpuInfo.protectedBuffers[existRegion][MPU_START_ADDRESS] == start \
+            or mpuInfo.protectedBuffers[existRegion][MPU_END_ADDRESS] == start:
+                mpuInfo.protectedBuffers[region][IF_START_PREDEFINED] = True
+                mpuInfo.protectedBuffers[region][MPU_START_ADDRESS] = existRegion
 
-            if mpu_info.bufName[existRegion][MPU_START_ADDRESS] == end \
-            or mpu_info.bufName[existRegion][MPU_END_ADDRESS] == end:
-                mpu_info.bufName[region][IF_END_PREDEFINED] = True
-                mpu_info.bufName[region][MPU_END_ADDRESS] = existRegion
+            if mpuInfo.protectedBuffers[existRegion][MPU_START_ADDRESS] == end \
+            or mpuInfo.protectedBuffers[existRegion][MPU_END_ADDRESS] == end:
+                mpuInfo.protectedBuffers[region][IF_END_PREDEFINED] = True
+                mpuInfo.protectedBuffers[region][MPU_END_ADDRESS] = existRegion
 
 
-    mpu_info.isEnabled[region] = True
+    mpuInfo.isEnabled[region] = True
 
-    if not mpu_info.bufName[region][IF_END_PREDEFINED]:
-        if not mpu_info.bufName[region][IF_START_PREDEFINED]:
+    if not mpuInfo.protectedBuffers[region][IF_END_PREDEFINED]:
+        if not mpuInfo.protectedBuffers[region][IF_START_PREDEFINED]:
             if None == end:
-                mpu_info.bufName[region] = [start, "_noEnd_", False, False]
+                mpuInfo.protectedBuffers[region] = [start, "_noEnd_", False, False]
             else:
-                mpu_info.bufName[region] = [start, end, False, False]
-        elif mpu_info.bufName[region][IF_START_PREDEFINED]:
+                mpuInfo.protectedBuffers[region] = [start, end, False, False]
+        elif mpuInfo.protectedBuffers[region][IF_START_PREDEFINED]:
             if None == end:
-                mpu_info.bufName[region][MPU_END_ADDRESS] = "_noEnd_"
+                mpuInfo.protectedBuffers[region][MPU_END_ADDRESS] = "_noEnd_"
             else:
-                mpu_info.bufName[region][MPU_END_ADDRESS] = end
+                mpuInfo.protectedBuffers[region][MPU_END_ADDRESS] = end
 
-    elif mpu_info.bufName[region][IF_END_PREDEFINED]:
-        if not mpu_info.bufName[region][IF_START_PREDEFINED]:
-            mpu_info.bufName[region][MPU_START_ADDRESS] = start
+    elif mpuInfo.protectedBuffers[region][IF_END_PREDEFINED]:
+        if not mpuInfo.protectedBuffers[region][IF_START_PREDEFINED]:
+            mpuInfo.protectedBuffers[region][MPU_START_ADDRESS] = start
 
 
 def setProtectedRegion(regions, sym=None, varibleName=None):
-    '''Configure the memory protected region
+    '''Configure the memory protected region,
         regions mean enabledRegions'''
+    mpuInfo = MPUInfo.Get()
 
     #varibale Name is None it means that it's for varibles start with "." (.dcpimage)
     if varibleName == None:
@@ -202,50 +212,51 @@ def setProtectedRegion(regions, sym=None, varibleName=None):
     sectDef = ""
     for region in regions:
         #Iterate every region
-        if not mpu_info.bufName[region][IF_END_PREDEFINED]:
-            if not mpu_info.bufName[region][IF_START_PREDEFINED]:
-                if sym in mpu_info.bufName[region]:
-                    if sym == mpu_info.bufName[region][0]:
+        if not mpuInfo.protectedBuffers[region][IF_END_PREDEFINED]:
+            if not mpuInfo.protectedBuffers[region][IF_START_PREDEFINED]:
+                if sym in mpuInfo.protectedBuffers[region]:
+                    if sym == mpuInfo.protectedBuffers[region][0]:
                         sectDef += "         _mpu_"+region + "_start = . ;\n"
                         sectDef += "        . = ALIGN({0:d});\n".format(4)
                         sectDef += "        *(" + varibleName + ")\n"
-                        if "_noEnd_" == mpu_info.bufName[region][MPU_END_ADDRESS]:
+                        if "_noEnd_" == mpuInfo.protectedBuffers[region][MPU_END_ADDRESS]:
                             sectDef += "         _mpu_"+region + "_end = . ;\n"
-                    elif sym == mpu_info.bufName[region][MPU_END_ADDRESS]:
+                    elif sym == mpuInfo.protectedBuffers[region][MPU_END_ADDRESS]:
                         sectDef += "        . = ALIGN({0:d});\n".format(4)
                         sectDef += "        *(" + varibleName + ")\n"
                         sectDef += "         _mpu_"+region + "_end = . ;\n"
 
-            elif mpu_info.bufName[region][IF_START_PREDEFINED]:
-                if sym == mpu_info.bufName[region][MPU_END_ADDRESS]:
+            elif mpuInfo.protectedBuffers[region][IF_START_PREDEFINED]:
+                if sym == mpuInfo.protectedBuffers[region][MPU_END_ADDRESS]:
                     sectDef += "        . = ALIGN({0:d});\n".format(4)
                     sectDef += "        *(" + varibleName + ")\n"
                     sectDef += "         _mpu_"+region + "_end = . ;\n"
 
-        if mpu_info.bufName[region][IF_END_PREDEFINED]:
-            if not mpu_info.bufName[region][IF_START_PREDEFINED]:
-                if sym in mpu_info.bufName[region]:
-                    if sym == mpu_info.bufName[region][0]:
+        if mpuInfo.protectedBuffers[region][IF_END_PREDEFINED]:
+            if not mpuInfo.protectedBuffers[region][IF_START_PREDEFINED]:
+                if sym in mpuInfo.protectedBuffers[region]:
+                    if sym == mpuInfo.protectedBuffers[region][0]:
                         sectDef += "         _mpu_"+region + "_start = . ;\n"
                         sectDef += "        . = ALIGN({0:d});\n".format(4)
                         sectDef += "        *(" + varibleName + ")\n"
-                        if "_noEnd_" == mpu_info.bufName[region][MPU_END_ADDRESS]:
+                        if "_noEnd_" == mpuInfo.protectedBuffers[region][MPU_END_ADDRESS]:
                             sectDef += "         _mpu_"+region + "_end = . ;\n"
-                    elif sym == mpu_info.bufName[region][MPU_END_ADDRESS]:
+                    elif sym == mpuInfo.protectedBuffers[region][MPU_END_ADDRESS]:
                         sectDef += "        . = ALIGN({0:d});\n".format(4)
                         sectDef += "        *(" + varibleName + ")\n"
                         sectDef += "         _mpu_"+region + "_end = . ;\n"
 
         if  sym == None:
             #If predefined
-            if mpu_info.bufName[region][IF_START_PREDEFINED]:
-                sectDef += "         _mpu_"+region + "_start = _mpu_"+ mpu_info.bufName[region][0]+ "_start ;\n"
-            if mpu_info.bufName[region][IF_END_PREDEFINED]:
-                sectDef += "         _mpu_"+region + "_end = _mpu_"+ mpu_info.bufName[region][MPU_END_ADDRESS]+ "_end ;\n"
+            if mpuInfo.protectedBuffers[region][IF_START_PREDEFINED]:
+                sectDef += "         _mpu_" +region + "_start = _mpu_" + mpuInfo.protectedBuffers[region][0]+ "_start ;\n"
+            if mpuInfo.protectedBuffers[region][IF_END_PREDEFINED]:
+                sectDef += ("         _mpu_" +region + "_end = _mpu_" +
+                            mpuInfo.protectedBuffers[region][MPU_END_ADDRESS]+ "_end ;\n")
 
             #When the addresses are defined as None
-            if mpu_info.bufName[region][MPU_START_ADDRESS] == "None" \
-            and mpu_info.bufName[region][MPU_END_ADDRESS] == "None":
+            if mpuInfo.protectedBuffers[region][MPU_START_ADDRESS] == "None" \
+            and mpuInfo.protectedBuffers[region][MPU_END_ADDRESS] == "None":
                 sectDef += "         _mpu_"+region + "_start = 0x0 ;\n"
                 sectDef += "         _mpu_"+region + "_end = 0x0 ;\n"
 
